f91581ca89157e9384c28d8c7c8b666b370cb307
Hector Siman <hectorlutero@hotmail.com>
2026-03-19 16:01:45 -0300

    fix: add missing motion import in Founders component

--- src/components/Founders/Founders.tsx
+++ src/components/Founders/Founders.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './Founders.css';
 import hectorImg from '../../assets/founders/hector.png';
 import rafaelImg from '../../assets/founders/rafael.png';
